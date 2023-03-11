ecaf08947af487ea15d8df21327d3217016ff91a
Mehdi Zibout <mehdizibout@gmail.com>
2023-03-11 18:13:27 +0100

    Add frontend React/vite with tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="w-screen h-screen bg-slate-700 text-3xl text-slate-200 flex justify-center items-center">
+      Hello everyone! welcome to the
+      <span className="font-bold px-2">Bug Tracker App</span>
+      frontend!
+    </div>
+  );
+}
+
+export default App;
