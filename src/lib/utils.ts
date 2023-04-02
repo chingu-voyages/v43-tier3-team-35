bd26ddbac191e24dc127352d102ec6ea43da379d
Mehdi Zibout <mehdizibout@gmail.com>
2023-04-02 16:03:28 +0000

    Add debounce hook

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,22 +1,54 @@
 import { clsx } from "clsx";
 import type { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { useEffect } from "react";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
 export function getNameLetters(name: string): string {
-    const words = name.split(" ")
-    switch (words.length) {
-      case 0:
-        return "AN"
-      case 1:
-        return words?.[0]?.[0] ?? "AN"
-      default:
-        const firstLetter = words?.[0]?.[0] ?? "A"
-        const secondLetter = words?.[1]?.[0] ?? "N"
-        return firstLetter + secondLetter;
-
-    }
+  const words = name.split(" ");
+  switch (words.length) {
+    case 0:
+      return "AN";
+    case 1:
+      return words?.[0]?.[0] ?? "AN";
+    default:
+      const firstLetter = words?.[0]?.[0] ?? "A";
+      const secondLetter = words?.[1]?.[0] ?? "N";
+      return firstLetter + secondLetter;
+  }
 }
+
+export function debounce<A = unknown, R = void>(
+  fn: (args: A) => R,
+  ms: number
+): [(args: A) => Promise<R>, () => void] {
+  let timer: NodeJS.Timeout;
+
+  const debouncedFunc = (args: A): Promise<R> =>
+    new Promise((resolve) => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+
+      timer = setTimeout(() => {
+        resolve(fn(args));
+      }, ms);
+    });
+
+  const teardown = () => clearTimeout(timer);
+
+  return [debouncedFunc, teardown];
+}
+
+export const useDebounce = <A = unknown, R = void>(
+  fn: (args: A) => R,
+  ms: number
+): ((args: A) => Promise<R>) => {
+  const [debouncedFun, teardown] = debounce<A, R>(fn, ms);
+
+  useEffect(() => () => teardown(), []);
+  return debouncedFun;
+};
