fb76399db71c5b8cb16cd8f425fec833c60a15d7
Mehdi Zibout <mehdizibout@gmail.com>
2023-04-04 02:57:00 +0000

    Fix bug, make current project a default value

--- src/pages/project/[id]/new.tsx
+++ src/pages/project/[id]/new.tsx
@@ -1,24 +1,24 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { type NextPage } from "next";
 import Link from "next/link";
-import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/24/outline"
+import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/24/outline";
 import { api } from "~/utils/api";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm, type SubmitHandler, Controller } from "react-hook-form";
 import { bugSchema } from "~/validation/bugInfo";
-import { cn } from "~/lib/utils"
+import { cn } from "~/lib/utils";
 import {
   Command,
   CommandEmpty,
   CommandGroup,
   CommandInput,
   CommandItem,
-} from "~/components/ui/command"
+} from "~/components/ui/command";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
-} from "~/components/ui/popover"
+} from "~/components/ui/popover";
 
 import {
   Select,
@@ -26,68 +26,95 @@ import {
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "~/components/ui/select"
-
+} from "~/components/ui/select";
+import { useRouter } from "next/router";
 
 const statuses = [
   { value: "LOW", label: "Low" },
   { value: "MEDIUM", label: "Medium" },
   { value: "HIGH", label: "High" },
   { value: "CRITICAL", label: "Critical" },
-]
+];
 
 type Inputs = {
-  title: string,
-  markdown: string,
-  priority: string,
-  projectId: string,
-}
+  title: string;
+  markdown: string;
+  priority: string;
+  projectId: string;
+};
 
 const NewBug: NextPage = () => {
-  const [open, setOpen] = useState(false)
-
-  const projects = api.project.getAll.useQuery();
-  const { mutate } = api.bugs.create.useMutation({
+  const [open, setOpen] = useState(false);
+  const {
+    query: { id },
+    push,
+  } = useRouter();
+  const projects = api.project.getTitles.useQuery();
+  const isValidProject = projects.data?.some((project) => project.id === id);
+  const { mutate } = api.bug.create.useMutation({
     onSuccess: () => {
-      console.log("success")
+      console.log("success");
     },
     onError: () => {
-      console.log("error")
-    }
+      console.log("error");
+    },
   });
 
-  const { control, register, handleSubmit, formState: { errors } } = useForm<Inputs>({
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>({
     resolver: zodResolver(bugSchema),
   });
 
-
   const onSubmit: SubmitHandler<Inputs> = (data) => {
     mutate(data);
-  }
-
+    void push(`/project/${id as string}`);
+  };
+  if (projects.isLoading) return <div className="">loading...</div>;
+  if (!isValidProject) return <div className="">error</div>;
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gray-900">
       <div className="container m-auto text-white">
-        <h1 className="text-white text-5xl mb-8">Report a bug</h1>
-        <form action="POST" onSubmit={handleSubmit(onSubmit)<void>}>
-          <div className="flex flex-row mb-4">
+        <h1 className="mb-8 text-5xl text-white">Report a bug</h1>
+        {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
+        <form action="POST" onSubmit={handleSubmit(onSubmit)}>
+          <div className="mb-4 flex flex-row">
             <div className="flex-auto pr-2">
-              <label className="block font-medium mb-2" htmlFor="">Title</label>
-              <input placeholder="" type="text" className="custom-input" {...register("title")} />
-              {errors.title && <p className="text-red-500 mt-2 font-medium">{errors.title.message}</p>}
+              <label className="mb-2 block font-medium" htmlFor="">
+                Title
+              </label>
+              <input
+                placeholder=""
+                type="text"
+                className="custom-input"
+                {...register("title")}
+              />
+              {errors.title && (
+                <p className="mt-2 font-medium text-red-500">
+                  {errors.title.message}
+                </p>
+              )}
             </div>
             <div className="flex-auto px-2">
-              <label className="block font-medium mb-2" htmlFor="">Project</label>
+              <label className="mb-2 block font-medium" htmlFor="">
+                Project
+              </label>
               <div>
                 <Controller
                   control={control}
                   name="projectId"
+                  defaultValue={id as string}
                   render={({ field: { value: fieldValue, onChange } }) => (
                     <Popover open={open} onOpenChange={setOpen}>
                       <PopoverTrigger asChild>
-                        <button className='custom-input text-left flex justify-between items-center'>
+                        <button className="custom-input flex items-center justify-between text-left">
                           {fieldValue && projects.data
-                            ? projects.data.find((project) => project.id === fieldValue)?.name
+                            ? projects.data.find(
+                                (project) => project.id === fieldValue
+                              )?.name
                             : "Select project..."}
                           <ChevronUpDownIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
                         </button>
@@ -97,23 +124,27 @@ const NewBug: NextPage = () => {
                           <CommandInput placeholder="Search project..." />
                           <CommandEmpty>No project found.</CommandEmpty>
                           <CommandGroup>
-                            {projects.data && projects.data.map((project) => (
-                              <CommandItem
-                                key={project.id}
-                                onSelect={(currentValue) => {
-                                  onChange(currentValue)
-                                  setOpen(false)
-                                }}
-                              >
-                                <CheckIcon
-                                  className={cn(
-                                    "mr-2 h-4 w-4",
-                                    fieldValue === project.id ? "opacity-100" : "opacity-0"
-                                  )}
-                                />
-                                {project.id}
-                              </CommandItem>
-                            ))}
+                            {projects.data &&
+                              projects.data.map((project) => (
+                                <CommandItem
+                                  key={project.id}
+                                  value={project.id}
+                                  onSelect={(currentValue) => {
+                                    onChange(currentValue);
+                                    setOpen(false);
+                                  }}
+                                >
+                                  <CheckIcon
+                                    className={cn(
+                                      "mr-2 h-4 w-4",
+                                      fieldValue === project.id
+                                        ? "opacity-100"
+                                        : "opacity-0"
+                                    )}
+                                  />
+                                  {project.name}
+                                </CommandItem>
+                              ))}
                           </CommandGroup>
                         </Command>
                       </PopoverContent>
@@ -121,18 +152,26 @@ const NewBug: NextPage = () => {
                   )}
                 />
               </div>
-              {errors.projectId && <p className="text-red-500 mt-2 font-medium">{errors.projectId.message}</p>}
+              {errors.projectId && (
+                <p className="mt-2 font-medium text-red-500">
+                  {errors.projectId.message}
+                </p>
+              )}
             </div>
             <div className="flex-auto px-2">
-              <label className="block font-medium mb-2" htmlFor="">Priority</label>
+              <label className="mb-2 block font-medium" htmlFor="">
+                Priority
+              </label>
               <div className="">
                 <Controller
                   control={control}
                   name="priority"
                   render={({ field: { onChange } }) => (
-                    <Select onValueChange={value => {
-                      onChange(value)
-                    }}>
+                    <Select
+                      onValueChange={(value) => {
+                        onChange(value);
+                      }}
+                    >
                       <SelectTrigger className="custom-input h-full">
                         <SelectValue placeholder="Select priority..." />
                       </SelectTrigger>
@@ -147,21 +186,43 @@ const NewBug: NextPage = () => {
                   )}
                 />
               </div>
-              {errors.priority && <p className="text-red-500 mt-2 font-medium">{errors.priority.message}</p>}
+              {errors.priority && (
+                <p className="mt-2 font-medium text-red-500">
+                  {errors.priority.message}
+                </p>
+              )}
             </div>
           </div>
           <div className="mb-3">
-            <label className="text-white font-medium mb-2 block" htmlFor="description">Description</label>
-            <textarea id="description" className="custom-input" rows={20} {...register("markdown")}></textarea>
-            {errors.markdown && <p className="text-red-500 mt-2 font-medium">{errors.markdown.message}</p>}
+            <label
+              className="mb-2 block font-medium text-white"
+              htmlFor="description"
+            >
+              Description
+            </label>
+            <textarea
+              id="description"
+              className="custom-input"
+              rows={20}
+              {...register("markdown")}
+            ></textarea>
+            {errors.markdown && (
+              <p className="mt-2 font-medium text-red-500">
+                {errors.markdown.message}
+              </p>
+            )}
           </div>
           <div className="ml-auto flex justify-end">
-            <Link href="/" className="btn mr-2">Cancel</Link>
-            <button type="submit" className="btn-blue">Submit</button>
+            <Link href="/" className="btn mr-2">
+              Cancel
+            </Link>
+            <button type="submit" className="btn-blue">
+              Submit
+            </button>
           </div>
         </form>
-      </div >
-    </main >
+      </div>
+    </main>
   );
 };
 
--- src/validation/bugInfo.ts
+++ src/validation/bugInfo.ts
@@ -2,8 +2,8 @@ import { z } from "zod";
 
 export const bugSchema = z.object({
   title: z.string().min(5, { message: "Title is too short" }).max(100),
-  markdown: z.string(),
+  markdown: z.string().min(5, { message: "Description is too short" }),
   priority: z.string(),
   projectId: z.string(),
-  duration: z.string(),
+  duration: z.string().optional(),
 });
